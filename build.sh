e71c9aeab4eeb741b07545c03334d7fdd70df7e4
Benjamin <78026082+benjamin-dcs@users.noreply.github.com>
2025-04-17 21:50:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,21 @@ DARKGRAY='\033[1;30m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+ON_WHITE='\033[47m' 
+
 clear
 
-echo -e "${RED}###${DARKGRAY} FORMATTING ${RED}###${NC}"
+echo -e "${RED}${ON_WHITE}###${DARKGRAY} FORMATTING ${RED}${ON_WHITE}###${NC}"
 npm run format
 echo ""
-echo -e "${RED}###${DARKGRAY} INSTALLING ${RED}###${NC}"
+echo -e "${RED}${ON_WHITE}###${DARKGRAY} INSTALLING ${RED}${ON_WHITE}###${NC}"
 npm install
 echo ""
-echo -e "${RED}###${DARKGRAY} CHECKING UNUSED DEPENDENCIES ${RED}###${NC}"
+echo -e "${RED}${ON_WHITE}###${DARKGRAY} CHECKING UNUSED DEPENDENCIES ${RED}${ON_WHITE}###${NC}"
 npm run depcheck
 echo ""
-echo -e "${RED}###${DARKGRAY} BUILDING ${RED}###${NC}"
+echo -e "${RED}${ON_WHITE}###${DARKGRAY} BUILDING ${RED}${ON_WHITE}###${NC}"
 npm run build
 echo ""
-echo -e "${RED}###${DARKGRAY} COPYING TO .hass_dev ${RED}###${NC}"
+echo -e "${RED}${ON_WHITE}###${DARKGRAY} COPYING TO .hass_dev ${RED}${ON_WHITE}###${NC}"
 cp dist/gauge-card-pro.js .hass_dev/www/gauge-card-pro
