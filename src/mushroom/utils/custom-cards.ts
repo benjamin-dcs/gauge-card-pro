05c9f29f34cede8bc2f2e74446e4b88a202a90f6
benjamin-dcs <78026082+benjamin-dcs@users.noreply.github.com>
2025-04-19 00:25:11 +0200

    Update custom-cards.ts
    Fix docs url

--- src/mushroom/utils/custom-cards.ts
+++ src/mushroom/utils/custom-cards.ts
@@ -15,6 +15,6 @@ export function registerCustomCard(params: RegisterCardParams) {
   windowWithCards.customCards.push({
     ...params,
     preview: true,
-    documentationURL: `${repository.url}/blob/main/docs/cards/${cardPage}.md`,
+    documentationURL: `${repository.url}/blob/main/README.md`,
   });
 }
