75bdc280fc7cd5219f7829573a8959bc9f9c415d
Benjamin <78026082+benjamin-dcs@users.noreply.github.com>
2025-05-10 10:46:50 +0200

    Improve icon handlers

--- src/tests/icon.test.ts
+++ src/tests/icon.test.ts
@@ -16,6 +16,21 @@ describe("isIcon", () => {
     const result = isIcon("3.14 kW");
     expect(result).toBe(false);
   });
+
+  it("number", () => {
+    const result = isIcon(123);
+    expect(result).toBe(false);
+  });
+
+  it("array", () => {
+    const result = isIcon(["icon(mdi:gauge)"]);
+    expect(result).toBe(false);
+  });
+
+  it("undefined", () => {
+    const result = isIcon(undefined);
+    expect(result).toBe(false);
+  });
 });
 
 describe("getIcon", () => {
@@ -32,5 +47,20 @@ describe("getIcon", () => {
   it("just text", () => {
     const result = getIcon("3.14 kW");
     expect(result).toEqual("3.14 kW");
+  });
+
+  it("number", () => {
+    const result = getIcon(123);
+    expect(result).toEqual(123);
+  });
+
+  it("array", () => {
+    const result = getIcon(["icon(mdi:gauge)"]);
+    expect(result).toEqual(["icon(mdi:gauge)"]);
+  });
+
+  it("undefined", () => {
+    const result = getIcon(undefined);
+    expect(result).toEqual(undefined);
   });
 });
