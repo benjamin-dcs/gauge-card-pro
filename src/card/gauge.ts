f854ce0c8a99b732d28c8cd6ceb5735c16499ae9
benjamin-dcs <78026082+benjamin-dcs@users.noreply.github.com>
2025-06-06 11:43:35 +0200

    Fix icon label flicker (#102)

--- src/card/gauge.ts
+++ src/card/gauge.ts
@@ -114,6 +114,7 @@ export class GaugeCardProGauge extends LitElement {
       this._updated = true;
       this._calculate_angles();
       this._rescaleValueTextSvg();
+      this._rescaleIconLabelTextSvg();
 
       if (this.shouldRenderGradient("main")) {
         this._mainGaugeGradient.initialize(
@@ -383,12 +384,12 @@ export class GaugeCardProGauge extends LitElement {
       this._rescaleValueTextSvg("secondary");
     }
 
-    if (this.iconIcon) {
+    if (changedProperties.has("iconLabel")) {
       this._rescaleIconLabelTextSvg();
     }
 
-    if (this.gradient && this.needle && this.gradientSegments) {
-      this._mainGaugeGradient.render(this.min, this.max, this.gradientSegments);
+    if (this.shouldRenderGradient("main")) {
+      this._mainGaugeGradient.render(this.min, this.max, this.gradientSegments!);
     }
 
     if (
@@ -431,6 +432,8 @@ export class GaugeCardProGauge extends LitElement {
   }
 
   private _rescaleIconLabelTextSvg() {
+    if (!this.iconIcon) return
+    
     const svgRoot = this.shadowRoot!.querySelector(".icon-label-text")!;
     const box = svgRoot.querySelector("text")!.getBBox()!;
     svgRoot.setAttribute(
