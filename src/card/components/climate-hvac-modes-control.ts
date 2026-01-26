35d3ae4ba55cea8c038f224281888e1028dc5957
Benjamin <78026082+benjamin-dcs@users.noreply.github.com>
2026-01-26 15:08:57 +0100

    Flash wanted hvac mode

--- src/card/components/climate-hvac-modes-control.ts
+++ src/card/components/climate-hvac-modes-control.ts
@@ -23,9 +23,12 @@ export class GCPClimateHvacModesControl extends LitElement {
 
   @property({ attribute: false }) public modes!: HvacMode[];
 
+  private wantedMode?: HvacMode;
+
   private _setHvacMode(e: CustomEvent) {
     e.stopPropagation();
     const mode = (e.target! as any).mode as HvacMode;
+    this.wantedMode = mode;
     this.hass.callService("climate", "set_hvac_mode", {
       entity_id: this.entity.entity_id,
       hvac_mode: mode,
@@ -43,17 +46,18 @@ export class GCPClimateHvacModesControl extends LitElement {
   private renderModeButton(mode: HvacMode) {
     const iconStyle = {};
     const color = mode === "off" ? "var(--grey-color)" : getHvacModeColor(mode);
-    if (mode === this.entity.state) {
+    const isPending = this.wantedMode === mode && this.wantedMode !== this.entity.state
+    if (mode === this.entity.state || isPending) {
       iconStyle["--icon-color"] = color;
       iconStyle["--bg-color"] = `color-mix(in srgb, ${color} 20%, transparent)`;
     }
-
     return html`
       <gcp-icon-button
         style=${styleMap(iconStyle)}
         appearance="circular"
         .mode=${mode}
         .disabled=${!isAvailable(this.entity)}
+        .pending=${isPending}
         .title=${localize(this.hass, `hvac_mode_titles.${mode}`)}
         @click=${this._setHvacMode}
       >
